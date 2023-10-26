fa12173ef343a1984cd4816b3c186063f8e6db33
Ariana H <huhkoa2@sunypoly.edu>
2023-10-26 00:36:21 -0400

    Added inner panels to panelNorth for buttons

--- Cycle2/NorthPanel.java
+++ Cycle2/NorthPanel.java
@@ -1,31 +1,24 @@
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
 
 public class NorthPanel {
 	static JPanel panelNorth;
 
 	public static JPanel Create() {
-		panelNorth = new JPanel();
+		panelNorth = new JPanel(new GridLayout(1,2));
 	    panelNorth.setBorder(new LineBorder(Color.BLACK, 2));
 	    panelNorth.setBackground(Color.WHITE);
-	    panelNorth.setLayout(new FlowLayout(5));
-        JTextArea fileContentTextArea = new JTextArea();	 
-        
-	    JButton fileButton = new JButton("Open File");
-	    fileButton.addActionListener(e -> {Buttons.openFile(fileContentTextArea);});
-	  
-        JLabel fileNameLabel = new JLabel("Selected file: ");
-	    
-        panelNorth.add(fileButton);
-        panelNorth.add(fileNameLabel);
-        panelNorth.add(fileContentTextArea);
-             
+	    panelNorth.setLayout(new BoxLayout(panelNorth, BoxLayout.LINE_AXIS));
+    
+        panelNorth.add(InnerPanel.Files());
+        panelNorth.add(Box.createHorizontalGlue());
+        panelNorth.add(InnerPanel.Calc());
+                  
 		return panelNorth;
 	}
 }
